b19a723634941791147c1ab8ee7f9e6aadf2d191
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-04-09 11:40:27 -0400

    3 tomcats servers running on 8081 8082 8083

--- main.tf
+++ main.tf
@@ -40,9 +40,9 @@ provider "nomad" {
   secret_id = data.vault_kv_secret_v2.bootstrap.data["SecretID"]
 }
 
-resource "nomad_job" "frontend" {
+resource "nomad_job" "java_monitor_method_1" {
   jobspec = <<EOT
-job "tomcat_hello_world" {
+job "java_monitor_method_1" {
     datacenters = ["dc1"]
     type = "service"
     node_pool = "x86"
@@ -50,12 +50,12 @@ job "tomcat_hello_world" {
     group "hello_world_group" {
         network {
             port "http" {
-                static = 8080
+                static = 8081
             }
         }
         
         service {
-            name = "tomcat-1"
+            name = "java_monitor_method_1"
             port = "http"
             address = "$${attr.unique.platform.aws.public-ipv4}"
 
@@ -82,7 +82,117 @@ job "tomcat_hello_world" {
 
             config {
                 command = "/bin/sh"
-                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && cd apache-tomcat-10.1.20/bin && ./catalina.sh run"]
+                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && sed -i 's/port="8080"/port="8081"/' apache-tomcat-10.1.20/conf/server.xml && .apache-tomcat-10.1.20/bin/catalina.sh run"]
+            }
+
+            resources {
+                cpu    = 500
+                memory = 256
+            }
+        }
+    }
+}
+EOT
+}
+
+resource "nomad_job" "java_monitor_method_2" {
+  jobspec = <<EOT
+job "java_monitor_method_2" {
+    datacenters = ["dc1"]
+    type = "service"
+    node_pool = "x86"
+
+    group "hello_world_group" {
+        network {
+            port "http" {
+                static = 8082
+            }
+        }
+        
+        service {
+            name = "java_monitor_method_2"
+            port = "http"
+            address = "$${attr.unique.platform.aws.public-ipv4}"
+
+            check {
+                name     = "http-check"
+                type     = "http"
+                path     = "/"
+                interval = "10s"
+                timeout  = "2s"
+                port     = "http"
+                method   = "GET"
+            }
+        }
+
+        task "deploy_tomcat" {
+            driver = "exec"
+
+            artifact {
+                source = "https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.20/bin/apache-tomcat-10.1.20.tar.gz"
+                options {
+                    archive = false # go-getter would extract this owned by root, we need nobody
+                }
+            }
+
+            config {
+                command = "/bin/sh"
+                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && sed -i 's/port="8080"/port="8082"/' apache-tomcat-10.1.20/conf/server.xml && .apache-tomcat-10.1.20/bin/catalina.sh run"]
+            }
+
+            resources {
+                cpu    = 500
+                memory = 256
+            }
+        }
+    }
+}
+EOT
+}
+
+resource "nomad_job" "java_monitor_method_3" {
+  jobspec = <<EOT
+job "java_monitor_method_3" {
+    datacenters = ["dc1"]
+    type = "service"
+    node_pool = "x86"
+
+    group "hello_world_group" {
+        network {
+            port "http" {
+                static = 8083
+            }
+        }
+        
+        service {
+            name = "java_monitor_method_3"
+            port = "http"
+            address = "$${attr.unique.platform.aws.public-ipv4}"
+
+            check {
+                name     = "http-check"
+                type     = "http"
+                path     = "/"
+                interval = "10s"
+                timeout  = "2s"
+                port     = "http"
+                method   = "GET"
+            }
+        }
+
+        task "deploy_tomcat" {
+            driver = "exec"
+
+            artifact {
+                source = "https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.20/bin/apache-tomcat-10.1.20.tar.gz"
+                options {
+                    archive = false # go-getter would extract this owned by root, we need nobody
+                }
+            }
+
+            config {
+                command = "/bin/sh"
+                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && sed -i 's/port="8080"/port="8083"/' apache-tomcat-10.1.20/conf/server.xml && .apache-tomcat-10.1.20/bin/catalina.sh run"]
             }
 
             resources {
