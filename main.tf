6aa7fe6bcd40e162dc1225dd55f1fe52917e7ab8
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-04-09 11:56:01 -0400

    back to 1

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ job "java-monitor-method-1" {
     group "hello_world_group" {
         network {
             port "http" {
-                static = 8081
+                static = 8080
             }
         }
         
@@ -82,117 +82,7 @@ job "java-monitor-method-1" {
 
             config {
                 command = "/bin/sh"
-                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && sed -i 's/port=\"8080\"/port=\"8081\"/' apache-tomcat-10.1.20/conf/server.xml && ./apache-tomcat-10.1.20/bin/catalina.sh run"]
-            }
-
-            resources {
-                cpu    = 500
-                memory = 256
-            }
-        }
-    }
-}
-EOT
-}
-
-resource "nomad_job" "java_monitor_method_2" {
-  jobspec = <<EOT
-job "java-monitor-method-2" {
-    datacenters = ["dc1"]
-    type = "service"
-    node_pool = "x86"
-
-    group "hello_world_group" {
-        network {
-            port "http" {
-                static = 8082
-            }
-        }
-        
-        service {
-            name = "java-monitor-method-2"
-            port = "http"
-            address = "$${attr.unique.platform.aws.public-ipv4}"
-
-            check {
-                name     = "http-check"
-                type     = "http"
-                path     = "/"
-                interval = "10s"
-                timeout  = "2s"
-                port     = "http"
-                method   = "GET"
-            }
-        }
-
-        task "deploy_tomcat" {
-            driver = "exec"
-
-            artifact {
-                source = "https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.20/bin/apache-tomcat-10.1.20.tar.gz"
-                options {
-                    archive = false # go-getter would extract this owned by root, we need nobody
-                }
-            }
-
-            config {
-                command = "/bin/sh"
-                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && sed -i 's/port=\"8080\"/port=\"8082\"/' apache-tomcat-10.1.20/conf/server.xml && ./apache-tomcat-10.1.20/bin/catalina.sh run"]
-            }
-
-            resources {
-                cpu    = 500
-                memory = 256
-            }
-        }
-    }
-}
-EOT
-}
-
-resource "nomad_job" "java_monitor_method_3" {
-  jobspec = <<EOT
-job "java-monitor-method-3" {
-    datacenters = ["dc1"]
-    type = "service"
-    node_pool = "x86"
-
-    group "hello_world_group" {
-        network {
-            port "http" {
-                static = 8083
-            }
-        }
-        
-        service {
-            name = "java-monitor-method-3"
-            port = "http"
-            address = "$${attr.unique.platform.aws.public-ipv4}"
-
-            check {
-                name     = "http-check"
-                type     = "http"
-                path     = "/"
-                interval = "10s"
-                timeout  = "2s"
-                port     = "http"
-                method   = "GET"
-            }
-        }
-
-        task "deploy_tomcat" {
-            driver = "exec"
-
-            artifact {
-                source = "https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.20/bin/apache-tomcat-10.1.20.tar.gz"
-                options {
-                    archive = false # go-getter would extract this owned by root, we need nobody
-                }
-            }
-
-            config {
-                command = "/bin/sh"
-                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && sed -i 's/port=\"8080\"/port=\"8083\"/' apache-tomcat-10.1.20/conf/server.xml && ./apache-tomcat-10.1.20/bin/catalina.sh run"]
+                args = ["-c", "cd local/ && tar xzf apache-tomcat-10.1.20.tar.gz && ./apache-tomcat-10.1.20/bin/catalina.sh run"]
             }
 
             resources {
